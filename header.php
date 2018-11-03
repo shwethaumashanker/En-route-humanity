7ef7e90e2d395d3b189e4dcd168b7be2502ef7b7
Dhanushikka Ravichandiran <32406179+dravicha@users.noreply.github.com>
2018-11-03 17:46:35 -0400

    header & footer

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>En Route Humanity</title>
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <!-- nav -->
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">En Route Humanity</a>
+      </div>
+    </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+  </nav>
+    
+  <!-- ./nav -->
+    
--- index.php
+++ index.php
@@ -1,11 +1,55 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+
+<?php include "header.php" ?>
+
+  <!-- main -->
+  <main class="container">
+  <h1 class="text-center">Welcome to En Route Humanity! <br><small>See good, Do good.</small></h1>
+
+    <div class="row">
+      <div class="col-md-6">
+        <h4>Login to start your contribution!</h4>
+
+        <!-- login form -->
+        <form method="post" action="home.php">
+          <div class="form-group">
+            <input class="form-control" type="text" name="username" placeholder="Username">
+          </div>
+
+          <div class="form-group">
+            <input class="form-control" type="password" name="password" placeholder="Password">
+          </div>
+
+          <div class="form-group">
+            <input class="btn btn-primary" type="submit" name="login" value="Login">
+          </div>
+        </form>
+        <!-- ./login form -->
+      </div>
+      <div class="col-md-6">
+        <h4>Don't have an account yet? Register!</h4>
+
+        <!-- register form -->
+        <form method="post" action="home.php">
+          <div class="form-group">
+            <input class="form-control" type="text" name="username" placeholder="Username">
+          </div>
+
+          <div class="form-group">
+            <input class="form-control" type="text" name="location" placeholder="Location">
+          </div>
+
+          <div class="form-group">
+            <input class="form-control" type="password" name="password" placeholder="Password">
+          </div>
+
+          <div class="form-group">
+            <input class="btn btn-success" type="submit" name="register" value="Register">
+          </div>
+        </form>
+        <!-- ./register form -->
+      </div>
+    </div>
+  </main>
+  <!-- ./main -->
+
+ <?php include "footer.php" ?>
